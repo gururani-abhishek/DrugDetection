55ecd21374524325c130a4e3326b9b38a55249d8
kehsihba <gururani.abhishek@gmail.com>
2023-02-04 23:31:01 +0530

    added fake and certified medicine activities

---
+++ app/src/main/java/com/example/drugdetection/CertifiedMedicine.kt
@@ -0,0 +1,11 @@
+package com.example.drugdetection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CertifiedMedicine : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_certified_medicine)
+    }
+}
---
+++ app/src/main/java/com/example/drugdetection/FakeMedicine.kt
@@ -0,0 +1,11 @@
+package com.example.drugdetection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FakeMedicine : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fake_medicine)
+    }
+}
