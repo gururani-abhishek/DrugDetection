f531ef3d17bbdd1fd4d36c77aad3cae242e69c0e
Abhishek Gururani <abhishekgururani@Abhisheks-MacBook-Pro.local>
2023-02-24 18:06:01 +0530

    worked on adminactivity

--- app/src/main/java/com/example/drugdetection/AdminActivity.kt
+++ app/src/main/java/com/example/drugdetection/AdminActivity.kt
@@ -2,10 +2,23 @@ package com.example.drugdetection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class AdminActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
+
+
+        val loginBtn = findViewById<Button>(R.id.loginBtn)
+        loginBtn.setOnClickListener {
+            val adminId = findViewById<EditText>(R.id.adminId)
+            val adminPass = findViewById<EditText>(R.id.adminPass)
+
+            Toast.makeText(this,"this is adminId : ${adminId.text}," +
+                    " and this is adminPass : ${adminPass.text}", Toast.LENGTH_LONG).show()
+        }
     }
 }
