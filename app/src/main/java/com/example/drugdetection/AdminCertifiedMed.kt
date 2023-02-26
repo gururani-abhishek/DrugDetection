16e7c835ed42c66817e8fcfc81a303ad797e4aa1
Abhishek Gururani <abhishekgururani@Abhisheks-MacBook-Pro.local>
2023-02-26 15:38:20 +0530

    authentication feature added, also connected to firebase

---
+++ app/src/main/java/com/example/drugdetection/AdminCertifiedMed.kt
@@ -0,0 +1,11 @@
+package com.example.drugdetection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdminCertifiedMed : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_certified_med)
+    }
+}
---
+++ app/src/main/java/com/example/drugdetection/AdminMedCheck.kt
@@ -0,0 +1,11 @@
+package com.example.drugdetection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdminMedCheck : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_med_check)
+    }
+}
