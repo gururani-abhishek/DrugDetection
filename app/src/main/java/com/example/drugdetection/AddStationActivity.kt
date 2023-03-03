6f174d8c3f8b21250ad81184bd5b9cab6cb878ae
Abhishek Gururani <abhishekgururani@Abhisheks-MacBook-Pro.local>
2023-03-03 14:49:02 +0530

    prototype final commit, I've added the ability to addstation and submitrepot

--- app/src/main/java/com/example/drugdetection/AddStationActivity.kt
+++ app/src/main/java/com/example/drugdetection/AddStationActivity.kt
@@ -2,7 +2,13 @@ package com.example.drugdetection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.widget.Button
+import android.widget.EditText
 import com.example.drugdetection.R
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class AddStationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +16,32 @@ class AddStationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_add_station)
 
         val uniqueId = intent.getStringExtra("UNIQUE_ID")
+        // using this uniqueId I'll fetch the document, add new station to it and then
+        // put it back to the database.
 
+        val submitBtn = findViewById<Button>(R.id.btn_submit)
+        submitBtn.setOnClickListener {
+
+            val etNameWidget = findViewById<EditText>(R.id.et_name)
+            val etIdWidget = findViewById<EditText>(R.id.et_id)
+
+            val stationName = etNameWidget.text.toString()
+            val stationId = etIdWidget.text.toString()
+
+            val db = Firebase.firestore
+            if (uniqueId != null) {
+                val docRef = db.collection("authMedicines").document(uniqueId)
+
+                docRef.update("stations", FieldValue.arrayUnion(stationName))
+                docRef.update("stationsId", FieldValue.arrayUnion(stationId))
+
+            }
+
+//            this doesn't work because, it expects an "Editable", and setText(setter) does the
+//            needful and converts empty string to Editable.
+//            etNameWidget.text = ""
+            etNameWidget.setText("")
+            etIdWidget.setText("")
+        }
     }
 }
--- app/src/main/java/com/example/drugdetection/FakeMedicine.kt
+++ app/src/main/java/com/example/drugdetection/FakeMedicine.kt
@@ -1,19 +1,47 @@
 package com.example.drugdetection
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class FakeMedicine : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fake_medicine)
 
         // extracting uniqueId from intent which redirected me to this activiy.
-        val intent = intent
         val uniqueId = intent.getStringExtra("UNIQUE_ID")
 
-//        Toast.makeText(this, "UNIQUE_ID = $uniqueId", Toast.LENGTH_LONG).show()
+        val btnReportSubmit = findViewById<Button>(R.id.btn_report_submit)
+        btnReportSubmit.setOnClickListener {
+            val reportId = uniqueId + "_report" // made a custom reportId
+
+
+            // get an instance of your firestore db
+            val db = Firebase.firestore
+
+            // extracting report from editview
+            val etReportWidget = findViewById<EditText>(R.id.et_report)
+
+            // my app was crashing because I was not providing reportData in the
+            // expected format, firestore expects data in a specific format
+            val reportData = hashMapOf(
+                "reportDes" to etReportWidget.text.toString()
+            )
+
+//             putting this report  in the database using set()
+            db.collection("reports").document(reportId)
+                .set(reportData)
+
+            etReportWidget.setText("")
+        }
 
     }
 }
